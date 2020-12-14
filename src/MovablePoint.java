db64ac5ac83546fc0dc1534e1438aa1b624423ba
KyPT2503 <62481511+KyPT2503@users.noreply.github.com>
2020-12-14 14:19:16 +0700

    baitap

---
+++ src/MovablePoint.java
@@ -0,0 +1,61 @@
+public class MovablePoint extends Point {
+    private float xSpeed = 0f;
+    private float ySpeed = 0f;
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint() {
+    }
+
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float[] getSpeed() {
+        float[] arr = new float[2];
+        arr[0] = this.xSpeed;
+        arr[1] = this.ySpeed;
+        return arr;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    public void move() {
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+    }
+}
